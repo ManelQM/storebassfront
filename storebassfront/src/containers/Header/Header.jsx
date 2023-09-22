3b95132c71f12f95e27406f39307ec10c310e92a
Manel <quemonmanel@gmail.com>
2023-09-22 19:00:11 +0200

    feat: header

---
+++ storebassfront/src/containers/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+ const Header = () => {
+
+    return (
+        <div className="headerAesthetics">
+            <h1>Esto es el Header</h1>
+        </div>
+    );
+ };
+
+ export default Header;
