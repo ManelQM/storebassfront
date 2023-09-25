95aac9da415ba2c8b94c26e72d9d07964bcb32ae
Manel <quemonmanel@gmail.com>
2023-09-25 11:00:55 +0200

    feat:home

---
+++ storebassfront/src/containers/Body/Body.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "../Home/Home";
+
+const Body = () => {
+  return (
+    <div className="bodyAesthetics">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default Body;
