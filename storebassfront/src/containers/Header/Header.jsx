386171ed1fad6092acf154d708987e912cb7472b
Manel <quemonmanel@gmail.com>
2023-09-26 19:08:25 +0200

    chore:stuff

--- storebassfront/src/containers/Header/Header.jsx
+++ storebassfront/src/containers/Header/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Header.css"
+
  const Header = () => {
 
     return (
         <div className="headerAesthetics">
-            <h1>Esto es el Header Cabrones</h1>
+     
         </div>
     );
  };
