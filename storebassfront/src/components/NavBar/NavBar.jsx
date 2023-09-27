eb758c3f0c319c16d0a2bbe2d34db2efaf6dd3fb
Manel <quemonmanel@gmail.com>
2023-09-27 18:21:59 +0200

    feat:navbar component

---
+++ storebassfront/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,19 @@
+import React, {useRef} from "react";
+import "./NavBar.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import NavBar from "react-bootstrap/Navbar";
+import { FaBars, FaTimes } from "react-icons"
+
+
+export const NavBar = () => {
+    return (
+        <>
+        <header>
+          <>
+          {/* <nav ref= */}
+          </>  
+        </header>
+        </>
+    )
+}
