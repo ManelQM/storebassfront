ba5cd01e7e9f82b085cc31042bea0bda78446b72
Manel <quemonmanel@gmail.com>
2023-10-05 21:10:06 +0200

    feat:navbar finnaly

--- storebassfront/src/components/NavBar/NavBar.jsx
+++ storebassfront/src/components/NavBar/NavBar.jsx
@@ -1,42 +1,42 @@
 import React, { useRef } from "react";
 import "./NavBar.css";
-import { FaBars,FaTimes } from "react-icons/fa";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 export const NavBar = () => {
   const navRef = useRef();
   const showNavbar = () => {
-    navRef.current.classList.toggle("responsive_nav")
-  }
+    navRef.current.classList.toggle("responsive_nav");
+  };
   return (
     <>
       <header>
         <>
-          <nav
-            ref={navRef}>
-            
-              <a href="/">Home</a>
-              <a href="/">Bass</a>
-              <a href="/">NewBass</a>
-            
-          
-                <a className="logoAesthetics" href="/">STOREBASS</a>
-            
-            
-                <a href="/">Cables</a>
-                <a href="/">Cabinets</a>
-                <a href="/">Amps</a>
-                <a className="loginAccessButton" href="/login">Log In</a>
-            
+          <nav ref={navRef}>
+            <a href="/">Home  </a>
+            <a href="/">Bass  </a>
+            <a href="/">NewBass  </a>
+
+            <a className="logoAesthetics" href="/">
+              STOREBASS
+            </a>
+
+            <a href="/">Cables</a>
+            <a href="/">Cabinets</a>
+            <a href="/">Amps</a>
+            <a className="loginAccessButton" href="/login">
+              Log In
+            </a>
+
             <button className="nav-btn nav-close-btn" onClick={showNavbar}>
-        <FaTimes/>
-      </button>
+              <FaTimes />
+            </button>
           </nav>
           <button className="nav-btn" onClick={showNavbar}>
-        <FaBars/>
-      </button>
+            <FaBars />
+          </button>
         </>
       </header>
     </>
   );
 };
- export default NavBar;
+export default NavBar;
