47f13063552f2765f4022061a9c152292dcfeb1b
Manel <quemonmanel@gmail.com>
2023-10-06 20:16:44 +0200

    login form

--- storebassfront/src/containers/Body/Body.jsx
+++ storebassfront/src/containers/Body/Body.jsx
@@ -2,12 +2,19 @@ import React from "react";
 import "./Body.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "../Home/Home";
+import Register from "../Register/Register";
+import Login from "../Login/Login";
+
+
+
 
 const Body = () => {
   return (
     <div className="bodyAesthetics">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register/>} />
+        <Route path="/login" element={<Login/>} />
       </Routes>
     </div>
   );
---
+++ storebassfront/src/containers/Login/Login.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import "./Login.css";
+import {NavBar} from "../../components/NavBar/NavBar";
+
+const Login = (props) => {
+    const [email, setEmail] = useState("");
+    const [pass, setPass] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email);
+    }
+
+    return (
+        <>
+        <NavBar/>
+        <div className="authAestheticsContainer">
+            <h2>Login</h2>
+            <form className="loginAestheticsForm" onSubmit={handleSubmit}>
+                <label htmlFor="email">email</label>
+                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+                <label htmlFor="password">password</label>
+                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="***********" id="password" name="password" />
+                <button type="submit">Log In</button>
+            </form>
+            <button className="registerButtonAesthetics" onClick={() => props.onFormSwitch("register")}>Don't have an account? Register here</button>
+        </div>
+        </>
+    )
+};
+
+export default Login; 
+
