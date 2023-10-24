1609405817df543a09264efec773e8bdce268126
Manel <quemonmanel@gmail.com>
2023-10-24 20:39:32 +0200

    profileCard component working

---
+++ storebassfront/src/components/ProfileCard/ProfileCard.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import "./ProfileCard.css";
+
+const ProfileCard = () => {
+    return(
+        <div className="profileCardAesthetics">
+            <div className="containerProfile container  container-fluid">
+        <h2 className="mt-5 ml-5">My Profile</h2>
+
+        <div className=" justify-content-around mt-5 user-info">
+          <div className="col-12 col-md-3">
+            <figure className="avatar avatar-profile">
+              <img className="rounded-circle img-fluid" src="" alt="" />
+            </figure>
+            <a
+              href="#"
+              id="edit_profile"
+              className="btn btn-primary btn-block my-5"
+            >
+              Edit Profile
+            </a>
+          </div>
+
+          <div className="col-12 col-md-5">
+            <h4>Full Name</h4>
+            <p>********</p>
+
+            <h4>Email Address</h4>
+            <p>********@gmail.com</p>
+
+            <a href="#" className="btn btn-danger btn-block mt-5">
+              My Orders
+            </a>
+
+            <a href="#" className="btn btn-primary btn-block mt-3">
+              Change Password
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="containerProfile container  container-fluid">
+        <h2 className="mt-5 ml-5">Address</h2>
+        <div className=" justify-content-around mt-5 user-info">
+          <div className="col-12 col-md-3">
+            <figure className="avatar avatar-profile">
+              <img className="rounded-circle img-fluid" src="" alt="" />
+            </figure>
+            <a
+              href="#"
+              id="edit_profile"
+              className="btn btn-primary btn-block my-5"
+            >
+              Edit Profile
+            </a>
+          </div>
+
+          <div className="col-12 col-md-5">
+            <h4>Full Name</h4>
+            <p>********</p>
+
+            <h4>Email Address</h4>
+            <p>********@gmail.com</p>
+
+            <a href="#" className="btn btn-danger btn-block mt-5">
+              My Orders
+            </a>
+
+            <a href="#" className="btn btn-primary btn-block mt-3">
+              Change Password
+            </a>
+          </div>
+        </div>
+      </div>
+        </div>
+    );
+};
+
+export default ProfileCard; 
--- storebassfront/src/containers/UserProfile/UserProfile.jsx
+++ storebassfront/src/containers/UserProfile/UserProfile.jsx
@@ -1,77 +1,12 @@
 import React from "react";
 import "./UserProfile.css";
+import ProfileCard from "../../components/ProfileCard/ProfileCard";
 
 const UserProfile = () => {
   return (
-    <div className="profileAesthetics">
-      <div className="containerProfile container  container-fluid">
-        <h2 className="mt-5 ml-5">My Profile</h2>
-
-        <div className=" justify-content-around mt-5 user-info">
-          <div className="col-12 col-md-3">
-            <figure className="avatar avatar-profile">
-              <img className="rounded-circle img-fluid" src="" alt="" />
-            </figure>
-            <a
-              href="#"
-              id="edit_profile"
-              className="btn btn-primary btn-block my-5"
-            >
-              Edit Profile
-            </a>
-          </div>
-
-          <div className="col-12 col-md-5">
-            <h4>Full Name</h4>
-            <p>********</p>
-
-            <h4>Email Address</h4>
-            <p>********@gmail.com</p>
-
-            <a href="#" className="btn btn-danger btn-block mt-5">
-              My Orders
-            </a>
-
-            <a href="#" className="btn btn-primary btn-block mt-3">
-              Change Password
-            </a>
-          </div>
-        </div>
-      </div>
-      <div className="containerProfile container  container-fluid">
-      <h2 className="mt-5 ml-5">Address</h2>
-        <div className=" justify-content-around mt-5 user-info">
-          <div className="col-12 col-md-3">
-            <figure className="avatar avatar-profile">
-              <img className="rounded-circle img-fluid" src="" alt="" />
-            </figure>
-            <a
-              href="#"
-              id="edit_profile"
-              className="btn btn-primary btn-block my-5"
-            >
-              Edit Profile
-            </a>
-          </div>
-
-          <div className="col-12 col-md-5">
-            <h4>Full Name</h4>
-            <p>********</p>
-
-            <h4>Email Address</h4>
-            <p>********@gmail.com</p>
-
-            <a href="#" className="btn btn-danger btn-block mt-5">
-              My Orders
-            </a>
-
-            <a href="#" className="btn btn-primary btn-block mt-3">
-              Change Password
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
+   
+      <ProfileCard/>
+   
   );
 };
 
