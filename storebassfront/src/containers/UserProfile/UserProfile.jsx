bde49715a41e32dc6cfda4d43771679affb55401
Manel <quemonmanel@gmail.com>
2023-10-24 11:34:15 +0200

    feat:more userprofile aesthetics now responsive

--- storebassfront/src/containers/UserProfile/UserProfile.jsx
+++ storebassfront/src/containers/UserProfile/UserProfile.jsx
@@ -4,32 +4,32 @@ import "./UserProfile.css"
 const UserProfile = () => {
     return(
         <div className="profileAesthetics">  
-        <div class="container  container-fluid" className="containerProfile">
+        <div className="containerProfile container  container-fluid">
            
-        <h2 class="mt-5 ml-5">My Profile</h2>
+        <h2 className="mt-5 ml-5">My Profile</h2>
   
-        <div class=" justify-content-around mt-5 user-info">
-            <div class="col-12 col-md-3">
-                <figure class='avatar avatar-profile'>
-                    <img class="rounded-circle img-fluid" src='' alt='' />
+        <div className=" justify-content-around mt-5 user-info">
+            <div className="col-12 col-md-3">
+                <figure className='avatar avatar-profile'>
+                    <img className="rounded-circle img-fluid" src='' alt='' />
                 </figure>
                 <a href="#" id="edit_profile" class="btn btn-primary btn-block my-5">
                     Edit Profile
                 </a>
             </div>
      
-            <div class="col-12 col-md-5">
+            <div className="col-12 col-md-5">
                  <h4>Full Name</h4>
                  <p>Ghulam Abbas</p>
      
                  <h4>Email Address</h4>
                  <p>ghulamabbas258@gmail.com</p>
 
-                 <a href="#" class="btn btn-danger btn-block mt-5">
+                 <a href="#" className="btn btn-danger btn-block mt-5">
                     My Orders
                 </a>
 
-                <a href="#" class="btn btn-primary btn-block mt-3">
+                <a href="#" className="btn btn-primary btn-block mt-3">
                     Change Password
                 </a>
             </div>
